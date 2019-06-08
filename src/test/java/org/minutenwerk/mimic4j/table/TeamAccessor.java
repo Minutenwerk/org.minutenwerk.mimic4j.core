8f0d8eb09ede2a3388e62f21cc1f906d3aeeb801
olaf.kossak <olaf.kossak@hamburg.de>
2019-06-08 12:25:28 +0200

    Introduction of model accessors

--- src/test/java/org/minutenwerk/mimic4j/table/TeamAccessor.java
+++ src/test/java/org/minutenwerk/mimic4j/table/TeamAccessor.java
@@ -1,6 +1,9 @@
 package org.minutenwerk.mimic4j.table;
 
+import java.util.List;
+
 import org.minutenwerk.mimic4j.impl.accessor.MmAttributeAccessor;
+import org.minutenwerk.mimic4j.impl.accessor.MmListAccessor;
 import org.minutenwerk.mimic4j.impl.accessor.MmRootAccessor;
 
 public class TeamAccessor extends MmRootAccessor<Team> {
@@ -9,8 +12,8 @@ public class TeamAccessor extends MmRootAccessor<Team> {
     return new MmAttributeAccessor<>(this, Team::getName, Team::setName);
   }
 
-  public PersonenAccessor personen() {
-    return new PersonenAccessor(this);
+  public MmListAccessor<Team, List<Person>, Person> personen() {
+    return new MmListAccessor<>(this, Team::getPersonen, Team::setPersonen);
   }
 
   public PersonAccessor person(final int index) {
