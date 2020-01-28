99daa04c679eec51ac7391b95994fc565842bc8d
olaf.kossak <olaf.kossak@hamburg.de>
2020-01-28 16:03:39 +0100

    RelationsshipApi erweitert

--- src/main/java/org/minutenwerk/mimic4j/api/MmRelationshipApi.java
+++ src/main/java/org/minutenwerk/mimic4j/api/MmRelationshipApi.java
@@ -1,7 +1,9 @@
 package org.minutenwerk.mimic4j.api;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.minutenwerk.mimic4j.impl.MmBaseDeclaration;
 import org.minutenwerk.mimic4j.impl.MmBaseImplementation;
 import org.minutenwerk.mimic4j.impl.MmInternal;
 
@@ -53,6 +55,26 @@ public class MmRelationshipApi {
   public static List<MmMimic> getMmChildren(MmMimic pMimic) {
     final MmBaseImplementation<?, ?, ?> implementationPartOfMimic = MmInternal.getMmImplementation(pMimic);
     return implementationPartOfMimic.getMmChildren();
+  }
+
+  /**
+   * Returns the list of all direct declaration children of specified mimic, which are instances of class <code>MmBaseDeclaration</code>, including
+   * runtime children.
+   *
+   * @param         pDeclarationType  The specified mimic.
+   *
+   * @return        The list of all direct children of specified mimic of type <code>MmBaseDeclaration</code>, including runtime children.
+   *
+   * @throws        IllegalArgumentException  In case of parameter is not a subclass of MmBaseDeclaration.
+   */
+  @SuppressWarnings("unchecked")
+  public static <T extends MmMimic> List<T> getDirectChildrenOfType(MmMimic pMimic, Class<T> pDeclarationType) {
+    if (!pDeclarationType.isInterface() && !(MmBaseDeclaration.class.isAssignableFrom(pDeclarationType))) {
+      throw new IllegalArgumentException("Class " + pDeclarationType.getSimpleName() + " is not a subclass of MmBaseDeclaration");
+    }
+    return (List<T>)(getMmChildren(pMimic).stream() //
+        .filter(child -> pDeclarationType.isAssignableFrom(child.getClass())) //
+        .collect(Collectors.toList()));
   }
 
   /**
